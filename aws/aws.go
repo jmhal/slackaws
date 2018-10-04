8110be1a6ecfbb01dbfee39c1645b0c98275f79c
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2018-10-04 13:49:18 -0300

    Esboço das funções para o AWS.

--- aws/aws.go
+++ aws/aws.go
@@ -1,2 +1,14 @@
 // Pacote com as funções para o AWS.
+package aws
+
+func CreateInstance() (string) {
+   // Vai criar uma instância. Na primeira versão, todos as opções (região, imagem do SO, etc) são definidas no código.
+   // Depois, podemos parametrizar por um arquivo de configuração.
+}
+
+func CreateUsers(users []string) (map[string]string) {
+   // Com a instância já criada, cria os usuários
+   // Talvez seja necessário uma variável global para guardar informações sobre a instância já criada.
+}
+
 
