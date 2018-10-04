943515f4fdb889e1b9dfeac211f31d26c5ce369b
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2018-10-04 13:40:39 -0300

    Esquema geral da primeira versão.

--- slack/slack.go
+++ slack/slack.go
@@ -1,10 +1,12 @@
 package slack
-//Alterei
-func UsersList(slackURL string) ([]string) {
-   users := []string{slackURL}
+
+func UsersList(slackURL string, workspaceToken string) ([]string) {
+   // Esta função deve ser implementada. Deve acessar a API do Slack e retornar uma lista de usuários do grupo.
+   users := []string{slackURL, workspaceToken}
    return users
 }
 
 func SendMessageToUser(user string, message string) (string) {
-    return user + ":" + message
+   // Envia uma mensagem para o usuário.
+   return user + ":" + message
 }
--- slackaws.go
+++ slackaws.go
@@ -4,13 +4,25 @@ import (
    "fmt"
    "os"
    "github.com/jmhal/slackaws/slack"
+   "github.com/jmhal/slackaws/aws"
 )
 
 func main() {
-   slackURL := os.Args[1]
-   fmt.Println(slackURL)
-   users := slack.UsersList(slackURL)
-   fmt.Println(users)
-   status := slack.SendMessageToUser("joaomarcelo", "teste")
-   fmt.Println(status)
+   // Na primeira versão, vamos começar passando dois parâmetros:
+   // - a url do grupo no slack (por exemplo, orientadosjm.slack.com)
+   // - o token de workspace da API para o mesmo grupo
+   slackWorkspaceURL := os.Args[1]
+   workspaceToken := os.Args[2]
+
+   // Recupera uma slice com todos os usuários
+   users := slack.UsersList(slackWorkspaceURL, workspaceToken)
+
+   // Recupera a URL de acesso público da instância e um map[nomedousuario->chave]
+   publicUrl := aws.CreateInstance()
+   sshKeys := aws.CreateUsers(users)
+
+   // Manda uma mensagem para cada usuário com o conteúdo da chave
+   for user, key := range sshKeys {
+      slack.SendMessage(user, key)
+   }
 }
