8b2a108ef956d9cd131fd5140a3eedc89a3e3dd9
danielalecio002 <danielalecio@outlook.com>
2018-10-10 16:15:43 -0300

    Atualizando aws

--- aws/aws.go
+++ aws/aws.go
@@ -1,9 +1,46 @@
 // Pacote com as funções para o AWS.
 package aws
+import (
+   "log"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/ec2"
+)
 
-func CreateInstance() (string) {
-   // Vai criar uma instância. Na primeira versão, todos as opções (região, imagem do SO, etc) são definidas no código.
-   // Depois, podemos parametrizar por um arquivo de configuração.
+func CreateInstance(Imagem string, Instancia string, NameInstancia string) {
+
+svc := ec2.New(session.New(&aws.Config{Region: aws.String("us-west-1")}))
+
+  runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
+
+        ImageId:      aws.String(Imagem),
+        InstanceType: aws.String(Instancia),
+        MinCount:     aws.Int64(1),
+        MaxCount:     aws.Int64(1),
+    })
+
+    if err != nil {
+        log.Println("Could not create instance", err)
+        return
+    }
+log.Println("Created instance", *runResult.Instances[0].InstanceId)
+ _ , errtag := svc.CreateTags(&ec2.CreateTagsInput{
+        Resources: []*string{runResult.Instances[0].InstanceId},
+        Tags: []*ec2.Tag{
+            {
+                Key:   aws.String("Name"),
+                Value: aws.String(NameInstancia),
+            },
+        },
+    })
+     if errtag != nil {
+        log.Println("Could not create tags for instance", runResult.Instances[0].InstanceId, errtag)
+        return
+    }
+
+    log.Println("Successfully tagged instance")
+
+
 }
 
 func CreateUsers(users []string) (map[string]string) {
