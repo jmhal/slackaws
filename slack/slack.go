e1e442de92e662196323dd8f0721e46b028fe0a2
jmhal <jmarcelo.alencar@gmail.com>
2018-10-10 10:47:48 -0300

    Alterei o arquivo slack.go para testar apenas a parte que já está pronta, a integração com o Slack. Removi o arquivo users_list.go, pois as funções foram migradas para o slack.go. Alterei a importação do framework no slack.go para evitar conflito de nomes. Consertei o paramêtro workspaceToken para apenas token.

--- slack/slack.go
+++ slack/slack.go
@@ -1,33 +1,35 @@
 package slack
 
+// Aqui estou importando o framework com o nome "bluele" para não entrar em conflito 
+// com o nosso próprio pacote que já tem o nome de "slack".
 import (
-	"github.com/bluele/slack"
+   bluele "github.com/bluele/slack"
 )
 
-func UsersList(workspaceToken string) ([]string) {
-	api := slack.New(token)
-	users, err := api.UsersList()
-	if err != nil {
-		panic(err)
-	}
-	foundUsers := make([]string, len(users))
-	for i := 0; i<len(users); i++ {
-		foundUsers[i] = users[i].Name
-	}
-	return foundUsers
+func UsersList(token string) ([]string) {
+   api := bluele.New(token)
+   users, err := api.UsersList()
+   if err != nil {
+      panic(err)
+   }
+   foundUsers := make([]string, len(users))
+   for i := 0; i<len(users); i++ {
+      foundUsers[i] = users[i].Name
+   }
+   return foundUsers
 }
 
-func SendMessageToUser(name string, message string, workspaceToken string) (string) {
-	api := slack.New(token)
-    users, err := api.UsersList()
-	if err != nil {
-		panic(err)
-	}
-    for _, user := range users {
-		if(user.Name == name) {
-			api.ChatPostMessage(user.Id, message, nil)
-		}
-	}
+func SendMessageToUser(name string, message string, token string) (string) {
+   api := bluele.New(token)
+   users, err := api.UsersList()
+   if err != nil {
+      panic(err)
+   }
+   for _, user := range users {
+      if(user.Name == name) {
+         api.ChatPostMessage(user.Id, message, nil)
+      }
+   }
    return name + ":" + message
 }
 
--- slackaws.go
+++ slackaws.go
@@ -4,25 +4,29 @@ import (
    "fmt"
    "os"
    "github.com/jmhal/slackaws/slack"
-   "github.com/jmhal/slackaws/aws"
+//   "github.com/jmhal/slackaws/aws"
 )
 
 func main() {
    // Na primeira versão, vamos começar passando dois parâmetros:
    // - a url do grupo no slack (por exemplo, orientadosjm.slack.com)
    // - o token de workspace da API para o mesmo grupo
-   slackWorkspaceURL := os.Args[1]
-   workspaceToken := os.Args[2]
+   // slackWorkspaceURL := os.Args[1]
+   workspaceToken := os.Args[1]
 
    // Recupera uma slice com todos os usuários
-   users := slack.UsersList(slackWorkspaceURL, workspaceToken)
+   users := slack.UsersList(workspaceToken)
+   fmt.Println(users)
 
+   slack.SendMessageToUser("joao.marcelo", "Teste direto do projeto", workspaceToken)
+
+   // Voltaremos a ativar a sequência abaixo quando finalizarmos o código AWS.
    // Recupera a URL de acesso público da instância e um map[nomedousuario->chave]
-   publicUrl := aws.CreateInstance()
-   sshKeys := aws.CreateUsers(users)
+   // publicUrl := aws.CreateInstance()
+   /// sshKeys := aws.CreateUsers(users)
 
    // Manda uma mensagem para cada usuário com o conteúdo da chave
-   for user, key := range sshKeys {
-      slack.SendMessage(user, key)
-   }
+   // for user, key := range sshKeys {
+   //   slack.SendMessage(user, key)
+   //}
 }
