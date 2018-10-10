f1176238fdcf5d2ada2bccdca27517346fb04094
Emanuel Douglas <email.emanueldouglas@gmail.com>
2018-10-09 21:09:27 -0300

    Correção de implementação (função: UserList)

--- slack/slack.go
+++ slack/slack.go
@@ -4,17 +4,17 @@ import (
 	"github.com/bluele/slack"
 )
 
-func UsersList(workspaceToken string) {
-	api := slack.New(workspaceToken)
+func UsersList(workspaceToken string) ([]string) {
+	api := slack.New(token)
 	users, err := api.UsersList()
 	if err != nil {
 		panic(err)
 	}
-	for _, user := range users {
-		if(user.Name != "slackbot" && user.Name != "slackaws") {
-			fmt.Println(user.Name)
-		}
+	foundUsers := make([]string, len(users))
+	for i := 0; i<len(users); i++ {
+		foundUsers[i] = users[i].Name
 	}
+	return foundUsers
 }
 
 func SendMessageToUser(name string, message string, workspaceToken string) (string) {
@@ -30,3 +30,4 @@ func SendMessageToUser(name string, message string, workspaceToken string) (stri
 	}
    return name + ":" + message
 }
+
--- slack/users_list.go
+++ slack/users_list.go
@@ -6,34 +6,24 @@ import (
 )
 
 const (
-	token = "add your token"
+	token = "xoxb-424210374210-425256236567-2ZNtzcbyZ4wNnvSuNucziZK5"
 )
 
 func main() {
-
-	UsersList(token);
-
-	/*
+	use := Lista(token)
+	for i := 0; i<len(use); i++ {
+		fmt.Println(use[i])
+	}
+}
+func Lista(workspaceToken string) ([]string) {
 	api := slack.New(token)
 	users, err := api.UsersList()
 	if err != nil {
 		panic(err)
 	}
-	for _, user := range users {
-		fmt.Println(user.Id, user.Name)
+	usuarios := make([]string, len(users))
+	for i := 0; i<len(users); i++ {
+		usuarios[i] = users[i].Name
 	}
-	*/
-}
-
-func UsersList(workspaceToken string) {
-	api := slack.New(workspaceToken)
-	users, err := api.UsersList()
-	if err != nil {
-		panic(err)
-	}
-	for _, user := range users {
-		if(user.Name != "slackbot" && user.Name != "slackaws") {
-			fmt.Println(user.Name)
-		}
-	}
+	return usuarios
 }
