6f4d987d491d2cbb358ea139cc56ee5076a553ff
Emanuel Douglas <email.emanueldouglas@gmail.com>
2018-10-09 21:12:56 -0300

    Remoção do token

--- slack/users_list.go
+++ slack/users_list.go
@@ -5,25 +5,9 @@ import (
 	"fmt"
 )
 
-const (
-	token = "xoxb-424210374210-425256236567-2ZNtzcbyZ4wNnvSuNucziZK5"
-)
-
 func main() {
 	use := Lista(token)
 	for i := 0; i<len(use); i++ {
 		fmt.Println(use[i])
 	}
-}
-func Lista(workspaceToken string) ([]string) {
-	api := slack.New(token)
-	users, err := api.UsersList()
-	if err != nil {
-		panic(err)
-	}
-	usuarios := make([]string, len(users))
-	for i := 0; i<len(users); i++ {
-		usuarios[i] = users[i].Name
-	}
-	return usuarios
 }
