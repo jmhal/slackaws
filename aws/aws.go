287e24c035bedfb2aa32823acea03c4f4096362e
João Marcelo Uchôa de Alencar <you@example.com>
2018-10-24 12:04:31 -0300

    Verificando aws.go

--- aws/aws.go
+++ aws/aws.go
@@ -2,146 +2,144 @@
 package aws
 import (
    "log"
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/ec2"
+   "fmt"
+   "io"
+   "io/ioutil"
+   "net"
+   "os"
+   "strings"
 
-    "fmt"
-    "io"
-    "io/ioutil"
-    "net"
-    "os"
-    "strings"
+   "github.com/aws/aws-sdk-go/aws"
+   "github.com/aws/aws-sdk-go/aws/session"
+   "github.com/aws/aws-sdk-go/service/ec2"
+   "github.com/bramvdbogaerde/go-scp"
+   "github.com/bramvdbogaerde/go-scp/auth"
 
-    "golang.org/x/crypto/ssh"
-    "golang.org/x/crypto/ssh/agent"
-    "github.com/bramvdbogaerde/go-scp"
-    "github.com/bramvdbogaerde/go-scp/auth"
-
+   "golang.org/x/crypto/ssh"
+   "golang.org/x/crypto/ssh/agent"
 )
 
 type SSHCommand struct {
-	Path   string
-	Env    []string
-	Stdin  io.Reader
-	Stdout io.Writer
-	Stderr io.Writer
+   Path   string
+   Env    []string
+   Stdin  io.Reader
+   Stdout io.Writer
+   Stderr io.Writer
 }
 
 type SSHClient struct {
-	Config *ssh.ClientConfig
-	Host   string
-	Port   int
+   Config *ssh.ClientConfig
+   Host   string
+   Port   int
 }
 
 func (client *SSHClient) RunCommand(cmd *SSHCommand) error {
-	var (
-		session *ssh.Session
-		err     error
-	)
+   var (
+      session *ssh.Session
+      err     error
+   )
 
-	if session, err = client.newSession(); err != nil {
-		return err
-	}
-	defer session.Close()
+   if session, err = client.newSession(); err != nil {
+      return err
+   }
+   defer session.Close()
 
-	if err = client.prepareCommand(session, cmd); err != nil {
-		return err
-	}
+   if err = client.prepareCommand(session, cmd); err != nil {
+      return err
+   }
 
-	err = session.Run(cmd.Path)
-	return err
+   err = session.Run(cmd.Path)
+   return err
 }
 
 func (client *SSHClient) prepareCommand(session *ssh.Session, cmd *SSHCommand) error {
-	for _, env := range cmd.Env {
-		variable := strings.Split(env, "=")
-		if len(variable) != 2 {
-			continue
-		}
+   for _, env := range cmd.Env {
+      variable := strings.Split(env, "=")
+      if len(variable) != 2 {
+         continue
+      }
 
-		if err := session.Setenv(variable[0], variable[1]); err != nil {
-			return err
-		}
-	}
+      if err := session.Setenv(variable[0], variable[1]); err != nil {
+	 return err
+      }
+   }
 
-	if cmd.Stdin != nil {
-		stdin, err := session.StdinPipe()
-		if err != nil {
-			return fmt.Errorf("Unable to setup stdin for session: %v", err)
-		}
-		go io.Copy(stdin, cmd.Stdin)
-	}
+   if cmd.Stdin != nil {
+      stdin, err := session.StdinPipe()
+      if err != nil {
+         return fmt.Errorf("Unable to setup stdin for session: %v", err)
+      }
+      go io.Copy(stdin, cmd.Stdin)
+   }
 
-	if cmd.Stdout != nil {
-		stdout, err := session.StdoutPipe()
-		if err != nil {
-			return fmt.Errorf("Unable to setup stdout for session: %v", err)
-		}
-		go io.Copy(cmd.Stdout, stdout)
-	}
+   if cmd.Stdout != nil {
+      stdout, err := session.StdoutPipe()
+      if err != nil {
+         return fmt.Errorf("Unable to setup stdout for session: %v", err)
+      }
+      go io.Copy(cmd.Stdout, stdout)
+   }
 
-	if cmd.Stderr != nil {
-		stderr, err := session.StderrPipe()
-		if err != nil {
-			return fmt.Errorf("Unable to setup stderr for session: %v", err)
-		}
-		go io.Copy(cmd.Stderr, stderr)
-	}
-
-	return nil
+   if cmd.Stderr != nil {
+      stderr, err := session.StderrPipe()
+      if err != nil {
+         return fmt.Errorf("Unable to setup stderr for session: %v", err)
+      }
+      go io.Copy(cmd.Stderr, stderr)
+   }
+   return nil
 }
 
 func (client *SSHClient) newSession() (*ssh.Session, error) {
-	connection, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", client.Host, client.Port), client.Config)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to dial: %s", err)
-	}
+   connection, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", client.Host, client.Port), client.Config)
+   if err != nil {
+      return nil, fmt.Errorf("Failed to dial: %s", err)
+   }
 
-	session, err := connection.NewSession()
-	if err != nil {
-		return nil, fmt.Errorf("Failed to create session: %s", err)
-	}
+   session, err := connection.NewSession()
+   if err != nil {
+      return nil, fmt.Errorf("Failed to create session: %s", err)
+   }
 
-	modes := ssh.TerminalModes{
-		// ssh.ECHO:          0,     // disable echoing
-		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
-		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
-	}
+   modes := ssh.TerminalModes{
+      // ssh.ECHO:          0,     // disable echoing
+      ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+      ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+   }
 
-	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
-		session.Close()
-		return nil, fmt.Errorf("request for pseudo terminal failed: %s", err)
-	}
+   if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+      session.Close()
+      return nil, fmt.Errorf("request for pseudo terminal failed: %s", err)
+   }
 
-	return session, nil
+   return session, nil
 }
 
 func PublicKeyFile(file string) ssh.AuthMethod {
-	buffer, err := ioutil.ReadFile(file)
-	if err != nil {
-		return nil
-	}
+   buffer, err := ioutil.ReadFile(file)
+   if err != nil {
+      return nil
+   }
 
-	key, err := ssh.ParsePrivateKey(buffer)
-	if err != nil {
-		return nil
-	}
-	return ssh.PublicKeys(key)
+   key, err := ssh.ParsePrivateKey(buffer)
+   if err != nil {
+      return nil
+   }
+   return ssh.PublicKeys(key)
 }
 
 func SSHAgent() ssh.AuthMethod {
-	if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
-		return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
-	}
-	return nil
+   if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+      return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
+   }
+   return nil
 }
 
-func SendFileToInstance(key string, url string){
+func SendScriptToInstance(key string, url string){
     clientSCPConfig, _ := auth.PrivateKey("ubuntu", key, ssh.InsecureIgnoreHostKey())
     clientSCP := scp.NewClient(url+":22", &clientSCPConfig)
 
-        err := clientSCP.Connect()
+    err := clientSCP.Connect()
     if err != nil {
         fmt.Println("Couldn't establisch a connection to the remote server ", err)
         return
@@ -155,52 +153,49 @@ func SendFileToInstance(key string, url string){
     // Finaly, copy the file over
     // Usage: CopyFile(fileReader, remotePath, permission)
     clientSCP.CopyFile(f, "/home/ubuntu/createUsers.sh", "0655")
-
-}
-func createUsers(url string, usuarios []string) {
-    key := "/home/daniel/Downloads/slackaws.pem";
-    SendFileToInstance(key, url);
-
-    sshConfig := &ssh.ClientConfig{
-            User: "ubuntu",
-            HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-            Auth: []ssh.AuthMethod{
-                PublicKeyFile(key),
-            },
-        }
-
-        client := &SSHClient{
-            Config: sshConfig,
-            Host:   url,
-            Port:   22,
-        }
-
-            for _, usuario := range usuarios {
-           file, err := os.Create("./" + usuario + ".pem")
-           if err != nil {
-              fmt.Println(err)
-           }
-           cmd := &SSHCommand{
-            Path:   "sudo ./createUsers.sh " + usuario,
-            Env:    []string{"LC_DIR=/"},
-            Stdin:  os.Stdin,
-            Stdout: file,
-            Stderr: os.Stderr,
-           }
-
-           if err := client.RunCommand(cmd); err != nil {
-            fmt.Fprintf(os.Stderr, "command run error: %s\n", err)
-            os.Exit(1)
-           }
-           file.Sync()
-           file.Close()
-        }
-
-
 }
 
-func CreateInstance(instanceImage string, instanceType string, instanceName string, region string, keyName string) {
+func CreateUsers(url string, key string, usuarios []string) {
+   SendScriptToInstance(key, url);
 
+   sshConfig := &ssh.ClientConfig{
+      User: "ubuntu",
+      HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+      Auth: []ssh.AuthMethod {
+         PublicKeyFile(key),
+      },
+   }
+
+   client := &SSHClient {
+      Config: sshConfig,
+      Host:   url,
+      Port:   22,
+   }
+
+   for _, usuario := range usuarios {
+      file, err := os.Create("./" + usuario + ".pem")
+      if err != nil {
+         fmt.Println(err)
+      }
+      log.Printf("Creating user: %s\n", usuario)
+      cmd := &SSHCommand{
+         Path:   "sudo ./createUsers.sh " + usuario,
+         Env:    []string{"LC_DIR=/"},
+         Stdin:  os.Stdin,
+         Stdout: file,
+         Stderr: os.Stderr,
+      }
+
+      if err := client.RunCommand(cmd); err != nil {
+         fmt.Fprintf(os.Stderr, "command run error: %s\n", err)
+         os.Exit(1)
+      }
+      file.Sync()
+      file.Close()
+   }
+}
+
+func CreateInstance(instanceImage string, instanceType string, instanceName string, region string, keyName string) (string) {
    svc := ec2.New(session.New(&aws.Config{Region: aws.String(region)}))
    runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
       ImageId:      aws.String(instanceImage),
@@ -216,7 +211,7 @@ func CreateInstance(instanceImage string, instanceType string, instanceName stri
 
    if err != nil {
       log.Println("Could not create instance", err)
-      return
+      return "noinstance"
    }
 
    log.Println("Created instance", *runResult.Instances[0].InstanceId)
@@ -232,7 +227,7 @@ func CreateInstance(instanceImage string, instanceType string, instanceName stri
 
    if errtag != nil {
       log.Println("Could not create tags for instance", runResult.Instances[0].InstanceId, errtag)
-      return
+      return "noinstance"
    }
    log.Println("Successfully tagged instance")
 
@@ -251,7 +246,7 @@ func CreateInstance(instanceImage string, instanceType string, instanceName stri
 	 }
       }
    }
-   log.Println(publicDns)
+   return publicDns
 }
 
 
